4c7d9fb1ba37d74136e6f6691ec654e81426e868
Andre Henshaw <andrehenshaw@protonmail.com>
2024-02-18 20:57:47 +0100

    unsuccessfully implemented -m option

--- ccwc/src/main.rs
+++ ccwc/src/main.rs
@@ -1,10 +1,18 @@
+#![allow(unused)]
+
+
 use std::{env, fs::File, io::Read};
+use std::collections::HashMap;
+
+
+
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
     let mut filename: File = File::open(&args[1]).expect("failed to open provided file by name");
     let command: &String = &args[2];
     let mut filebuf: Vec<u8> = vec![];
+    let mut char_counter: HashMap<String, i32> = HashMap::new();
 
     match command as &str {
         "-c" => {
@@ -12,11 +20,21 @@ fn main() {
             println!("{:?} bytes", &data);
         }
         "-m" | "--chars" => {
+
+            let temp_buf = filebuf.clone();
+            println!("{:?}", &temp_buf);
+           if let Ok(ch) = String::from_utf8(temp_buf){
+                println!("{:?}",ch.chars().count());
+            };
+
             println!("print the character counts");
         }
+    },
         "-l" | "--lines" => {
+
+
             println!("print the newline counts");
-        }
+        },
 
         "-L" | "--max-line-length" => {
             println!("print the maximum display width");
